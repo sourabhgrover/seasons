ddc5a411bf8ea83e17e9b12286d6877c32e63884
sourabh <sourabh.grover@trantorinc.com>
2019-06-27 19:21:37 +0530

    Updates

---
+++ precache-manifest.15961faa322aac62277b3ba6098fb275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ee06fab4a0c48d80bb6",
+    "url": "/seasons/static/js/runtime~main.8ee06fab.js"
+  },
+  {
+    "revision": "6b7899f37c2e3a7622f6",
+    "url": "/seasons/static/js/main.6b7899f3.chunk.js"
+  },
+  {
+    "revision": "180d67520b117afbc2ab",
+    "url": "/seasons/static/js/2.180d6752.chunk.js"
+  },
+  {
+    "revision": "6b7899f37c2e3a7622f6",
+    "url": "/seasons/static/css/main.bfdb963c.chunk.css"
+  },
+  {
+    "revision": "c2a6c38abce90eccb3d13b2dd06caea8",
+    "url": "/seasons/index.html"
+  }
+];
